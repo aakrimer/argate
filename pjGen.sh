03b5364abb7f2d6ab4cca0d3499a34e17f57574f
Aarong Krimer <aakrimer@gmail.com>
2015-05-28 21:40:36 -0700

    added programs and scripts

---
+++ pjGen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "{ \
+  \"name\": \"naive\", \
+  \"description\": \"A package using naive versioning\", \
+  \"author\": \"A confused individual <iam@confused.com>\", \
+  \"dependencies\": {"
+
+for i in $(grep require *|awk -F \' '{print $2}'|sort -u|egrep -v '^\s*$')
+do
+    echo "\"$i"\": "\"x\"", \
+done
+echo "}, \
+  \"engine\": \"node 0.4.1\" \
+}"
