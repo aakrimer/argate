16d1d6f730490105442e3fe4cf1e0b6ce55391b5
Aarong Krimer <aakrimer@gmail.com>
2015-05-28 22:00:50 -0700

    fixed syntax

--- pjGen.sh
+++ pjGen.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 
-echo "{ \
+echo  "{ \
   \"name\": \"naive\", \
   \"description\": \"A package using naive versioning\", \
   \"author\": \"A confused individual <iam@confused.com>\", \
   \"dependencies\": {"
 
-for i in $(grep require *|awk -F \' '{print $2}'|sort -u|egrep -v '^\s*$')
+for i in $(grep require *.js|awk -F \' '{print $2}'|sort -u|egrep -v '^\s*$')
 do
-    echo "\"$i"\": "\"x\"", \
+    echo "\"$i"\": "\"x\","
 done
 echo "}, \
-  \"engine\": \"node 0.4.1\" \
-}"
+  \"engine\": \"node 0.4.1\" }"
